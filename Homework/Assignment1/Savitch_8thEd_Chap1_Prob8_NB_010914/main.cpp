f1776bffa9fb889797cb45ea258dad855e3edc30
ttendohv <vhodnett1@student.rcc.edu>
2014-01-09 20:53:56 -0800

    Signed-off-by: ttendohv <vhodnett1@student.rcc.edu>

---
+++ Homework/Assignment1/Savitch_8thEd_Chap1_Prob8_NB_010914/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Victoria Hodnett
+ * Created on January 9, 2014, 7:39 PM
+ * Savitch 8th Edition Chapter 1 Problem 8
+ * Calculate money in cents from quarters,
+ * dimes, and nickels
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+const short QTR_CNT = 25;
+const short DIM_CNT = 10;
+const short NIK_CNT = 5;
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare variables
+    int numQtr, numDime, numNikl, total;
+    //Input amount of quarters, dimes, and nickels
+    cout << "Please enter the following:\n Number of Quarters: ";
+    cin >> numQtr;
+    cout << " Number of Dimes: ";
+    cin >> numDime;
+    cout << " Number of Nickels: ";
+    cin >> numNikl;
+    //Calculate the total amount of money
+    total = (numQtr*QTR_CNT) + (numDime*DIM_CNT) + (numNikl*NIK_CNT);
+    //Output total
+    cout << "You have " << total << " cents.\nGoodbye";
+    //Exit
+    return 0;
+}
+
