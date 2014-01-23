910e546f79bbacef3d39907ebf5d1992f9f7cea5
ttendohv <vhodnett1@student.rcc.edu>
2014-01-23 09:48:24 -0800

    Signed-off-by: ttendohv <vhodnett1@student.rcc.edu>

---
+++ Savitch_8thEd_Chap4_Prob16_NB_012314/main.cpp
@@ -0,0 +1,61 @@
+/* 
+ * File:   main.cpp
+ * Author: Victoria Hodnett
+ * Created on January 23, 2014, 8:41 AM
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Global Constants
+const unsigned char CNV_MIN_HRS = 60; //60 minutes in an hour
+const unsigned char CNV_24HR = 12; //24 hour clock conversion 
+
+//Function Prototypes
+void read(unsigned short&, unsigned short&, char&);
+unsigned short minutes(unsigned short, unsigned short, char);
+unsigned short diff(unsigned short, unsigned short);
+
+//Execution Begins
+int main(int argc, char** argv) {
+    //Declare variables
+    unsigned short shr, ehr, smin, emin, scmin, ecmin, diffMin;
+    char sampm, eampm;
+    //Read the data
+    read(shr, smin, sampm);
+    read(ehr, emin, eampm);
+    //Convert to minutes
+    scmin = minutes(shr, smin, sampm);
+    ecmin = minutes(ehr, emin, eampm);
+    cout << "Starting Minutes = " << scmin << endl;
+    cout << "Ending Minutes = " << ecmin << endl;
+    //What is the difference in minutes?
+    diffMin = diff(scmin,ecmin);
+    cout << "The difference in minutes = " << diffMin << endl;
+    //Exit
+    return 0;
+}
+
+void read(unsigned short& hr, unsigned short& min, char& ampm){
+    char dum;
+    cout << "Input a time: " << endl;
+    cout << "Format XX:XX AM/PM" << endl;
+    cin >> setw(2) >> hr >> dum >> min >> setw(2) >> ampm >> dum;
+    cout << "You input the following: " << endl;
+    cout << "The hour = " << hr << endl;
+    cout << "The minute = " << min << endl;
+    cout << "AM/PM = " << ampm << "M" << endl;
+}
+
+unsigned short minutes(unsigned short hr, unsigned short min, char ampm){
+    hr += (((ampm = 'P' || ampm == 'p') && hr != CNV_24HR) ? CNV_24HR : 0);
+    hr -= (((ampm = 'A' || ampm == 'a') && hr == CNV_24HR) ? CNV_24HR : 0);
+    return (hr * CNV_MIN_HRS) + min;
+}
+
+unsigned short diff(unsigned short strt, unsigned short stp){
+    short min = stp - strt;
+    return (min >= 0)? min : (min + (2*CNV_24HR*CNV_MIN_HRS));
+}
